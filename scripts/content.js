3fd98ba9949797e6c68593724665f06b11376427
Alfred Kristal Ern <alfred.kristal@gmail.com>
2023-01-17 23:14:53 +0100

    add todos

--- scripts/content.js
+++ scripts/content.js
@@ -1,13 +1,10 @@
-// Quickfix to let docuemnts small enough have time to load before removing the paywall.
-// To make this a bit more robust, we should detect when a page is loaded and only then
-// remove the blur from it.
-
-
+// TODO: Find a more robust way to deal with these.
 const paywall = document.querySelector('._60e676a00207');
 if (paywall != null) {
     paywall.style.display = 'none';
 }
 
+// TODO: Find a more robust way to deal with these.
 const overlayItems = document.querySelectorAll('._869f7c361ca9');
 if (overlayItems != null) {
     overlayItems.forEach(item => item.style.display = 'none');
