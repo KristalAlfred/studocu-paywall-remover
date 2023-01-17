0dad73ccb491ccd06489253130d63c2f4e7bdefe
Alfred Kristal Ern <alfred.kristal@gmail.com>
2023-01-17 22:56:28 +0100

    add fix for lazy-loading issue

--- scripts/content.js
+++ scripts/content.js
@@ -1,27 +1,42 @@
 // Quickfix to let docuemnts small enough have time to load before removing the paywall.
 // To make this a bit more robust, we should detect when a page is loaded and only then
 // remove the blur from it.
-setTimeout(() => {
-    const documentWrapper = document.getElementById('document-wrapper');
-    if (documentWrapper != null) {
-        documentWrapper.parentNode.replaceChild(documentWrapper.cloneNode(true), documentWrapper);
-    }
 
-    const paywall = document.querySelector('._60e676a00207');
-    if (paywall != null) {
-        paywall.style.display = 'none';
-    }
 
-    const overlayItems = document.querySelectorAll('._869f7c361ca9');
-    if (overlayItems != null) {
-        overlayItems.forEach(item => item.style.display = 'none');
-    }
+const paywall = document.querySelector('._60e676a00207');
+if (paywall != null) {
+    paywall.style.display = 'none';
+}
 
-    const pages = document.querySelectorAll('.page-content');
-    if (pages != null) {
-        pages.forEach(page => {
-            page.style.filter = 'none';
-            page.style.removeProperty('filter');
-        });
+const overlayItems = document.querySelectorAll('._869f7c361ca9');
+if (overlayItems != null) {
+    overlayItems.forEach(item => item.style.display = 'none');
+}
+
+
+const config = { childList: true };
+
+const callback = (mutationList, _) => {
+    for (const mutation of mutationList) {
+        if (mutation.addedNodes.length > 0) {
+            const element = mutation.target;
+            setTimeout(() => {
+                if (element.children.length > 0) {
+                    const copy = element.cloneNode(true);
+                    copy.style.filter = 'none';
+                    copy.style.removeProperty('filter');
+                    element.parentNode.replaceChild(copy, element);
+                }
+            }, 200);
+        }
     }
-}, 200);
+};
+
+const observer = new MutationObserver(callback);
+
+const pages = document.querySelectorAll('.page-content');
+if (pages != null) {
+    pages.forEach(page => {
+        observer.observe(page, config);
+    });
+}
