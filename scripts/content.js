738ec5caa43a39ae54c4377342d8a5147cba1cbb
Alfred Kristal Ern <alfred.kristal@gmail.com>
2023-01-22 18:38:50 +0100

    take a more simple approach to remove blur

--- scripts/content.js
+++ scripts/content.js
@@ -10,32 +10,9 @@ if (overlayItems != null) {
     overlayItems.forEach(item => item.style.display = 'none');
 }
 
-
-const config = { childList: true };
-const callback = (mutationList, _) => {
-    for (const mutation of mutationList) {
-        if (mutation.addedNodes.length > 0) {
-            const element = mutation.target;
-
-            // Wait a bit for the content to load.
-            setTimeout(() => {
-                const contentLoaded = element.children.length > 0;
-                if (contentLoaded) {
-                    const copy = element.cloneNode(true);
-                    copy.style.filter = 'none';
-                    copy.style.removeProperty('filter');
-                    element.parentNode.replaceChild(copy, element);
-                }
-            }, 200);
-        }
-    }
-};
-
-const observer = new MutationObserver(callback);
-
-const pages = document.querySelectorAll('.page-content');
+const pages = document.querySelectorAll(".page-content");
 if (pages != null) {
     pages.forEach(page => {
-        observer.observe(page, config);
+        page.classList.add("no-blur");
     });
 }
